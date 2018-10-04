76018f980d0fd16d6f6eb2b84cb30a9032f0b782
Matt Mackowski <matt@mackowski.com>
2018-10-03 19:42:30 -0500

    Model for list Items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,4 @@
+from lists.models import Item
 from django.template.loader import render_to_string
 from django.urls import resolve
 from django.test import TestCase
@@ -5,8 +6,25 @@ from django.http import HttpRequest
 
 from lists.views import home_page
 
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
+
 class HomePageTest(TestCase):
-    
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
@@ -20,15 +38,15 @@ class HomePageTest(TestCase):
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
 
-        # # request = HttpRequest()
-        # # response = home_page(request)
-        # html = response.content.decode('utf8')
-        # # expected_html = render_to_string('home.html')
+        # request = HttpRequest()
+        # response = home_page(request)
+        html = response.content.decode('utf8')
+        # expected_html = render_to_string('home.html')
 
-        # self.assertTrue(html.startswith('<html>'))
-        # self.assertIn('<title>To-Do lists</title>', html)
-        # self.assertTrue(html.strip().endswith('</html>'))
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.strip().endswith('</html>'))
 
-        # self.assertTemplateUsed(response, 'home.html')
+        self.assertTemplateUsed(response, 'home.html')
 
 # Create your tests here.
